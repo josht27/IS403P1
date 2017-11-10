81f320c19c20f2f1f3164ce25d7b69dcd67f6ff1
Joshua Topacio <joshuatopacio@byu.edu>
2017-11-09 21:39:21 -0700

    Action Controller

---
+++ IS403P1/Controllers/ActionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IS403P1.Controllers
+{
+    public class ActionController : Controller
+    {
+        // GET: Action
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
